2c4935e62fb9b6c31245034cb06af578428cbe25
Yuto Nishida <yuyu.nishida@gmail.com>
2020-05-07 23:40:47 -0700

    Simple metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `&ref`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Yuto Nishida`,
+      summary: `.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `Low quality thought dump from a local shower near you!`,
+    siteUrl: `https://starptr.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `starptr`,
     },
   },
   plugins: [
