15dca8ac6c4f04356f89dca88aa05cfd61bbaa19
starptr <yuyu.nishida@gmail.com>
2020-06-10 14:31:16 -0700

    Fix gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,10 @@
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.createPages = async ({ graphql, actions }) => {
-	const { createPage } = actions;
-
-	const blogPost = path.resolve(`./src/templates/blog-post.js`);
-	const result = await graphql(
-		`
-			{
-				allMdx(
-					sort: { fields: [frontmatter___date], order: DESC }
-					limit: 1000
-				) {
-					edges {
-						node {
-							fields {
-								slug
-							}
-							frontmatter {
-								title
-							}
-						}
-					}
-				}
-			}
-		`
-	);
-
-	if (result.errors) {
-		throw result.errors;
-	}
-
-	// Create blog posts pages.
-	const posts = result.data.allMdx.edges;
-
-	posts.forEach((post, index) => {
-		const previous =
-			index === posts.length - 1 ? null : posts[index + 1].node;
-		const next = index === 0 ? null : posts[index - 1].node;
-
-		createPage({
-			path: post.node.fields.slug,
-			component: blogPost,
-			context: {
-				slug: post.node.fields.slug,
-				previous,
-				next,
-			},
-		});
-	});
-};
-
 exports.onCreateNode = ({ node, actions, getNode }) => {
 	const { createNodeField } = actions;
 
+	/*
 	if (node.internal.type === `Mdx`) {
 		const relativeFilePath = createFilePath({ node, getNode });
 		createNodeField({
@@ -62,4 +13,5 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 			value: `/blog${relativeFilePath}`,
 		});
 	}
+	*/
 };
