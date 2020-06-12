4557b7a040467fd8363275ca6535dd41319a56e1
starptr <yuyu.nishida@gmail.com>
2020-06-12 01:20:38 -0700

    Polish the main page and create amc-lp page

---
+++ src/pages/22to7/amc-lp/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { graphql } from "gatsby";
+import type { PageProps } from "gatsby";
+
+import SEO from "../../../components/seo";
+import Layout from "../../../components/layout";
+import { rhythm } from "../../../utils/typography";
+
+type Data = {
+	site: {
+		siteMetadata: {
+			title: string;
+		};
+	};
+};
+
+const Index = (props: PageProps<Data>) => {
+	const siteTitle = props.data.site.siteMetadata.title;
+	const title = "AMC: Let's Play!";
+
+	return (
+		<Layout location={props.location} title={siteTitle}>
+			<SEO title={title} />
+			<h1>{title}</h1>
+			<p>
+				Welcome! Here, you will find the latest scheduling of <strong>{title}</strong> livestreams.
+			</p>
+			<iframe
+				src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=a2luaXVzZWQyc2psZTVkY2J0dmJnOWdiNWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTitle=0&amp;showNav=1&amp;showDate=0&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=1&amp;showTz=1"
+				width="100%"
+				height="600"
+				scrolling="no"
+				style={{
+					borderWidth: 0,
+				}}
+			/>
+			<p
+				style={{
+					color: "red",
+				}}
+			>
+				Heads up! The info below may be old. If there is any inconsistency between the calendar above and the rest of the page, the
+				calendar is probably right.
+			</p>
+			<p>Join the call: <a href="https://meet.google.com/eyb-qyvq-nvk">Google Meet (eyb-qyvq-nvk)</a></p>
+			<table>
+				<thead>
+					<tr>
+						<td>Homework</td>
+						<td>By Date</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<p style={{ color: "lightgray", marginBottom: 0 }}>None</p>
+						</td>
+						<td>June 8</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="https://artofproblemsolving.com/wiki/index.php/2000_AMC_10_Problems">AMC 10 2000</a>
+						</td>
+						<td>June 16</td>
+					</tr>
+				</tbody>
+			</table>
+		</Layout>
+	);
+};
+
+export default Index;
+
+export const pageQuery = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+				author {
+					name
+				}
+				blog {
+					title
+				}
+			}
+		}
+	}
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import type { PageProps } from "gatsby";
 
 import Bio from "../components/bio";
@@ -8,6 +8,7 @@ import SEO from "../components/seo";
 import SmallBoxed from "../components/SmallBoxed";
 import Home from "../components/Home";
 import Blog from "../components/Blog";
+import Link from "../components/Link";
 import { rhythm } from "../utils/typography";
 
 type Data = {
@@ -32,8 +33,48 @@ const Index = (props: PageProps<Data>) => {
 		<Layout location={props.location} title={siteTitle}>
 			<SEO title="Home" />
 			<p>
-				👋Hi!! Welcome to <Home />. I'm <strong>{name}</strong>. Checkout my blog <Blog />.
+				👋Hi!! Welcome to <Home />. I'm <strong>{name}</strong>. I like to make things. This page lists some things I've made.
 			</p>
+			<p>Some interesting things I'm actively working on:</p>
+			<ul>
+				<li>
+					<strong>
+						<Link to="/">This website.</Link>
+					</strong>{" "}
+					It's far from polished, and eventually I'd like to put most of my work here.
+				</li>
+				<li>
+					<strong>
+						<Link to="/22to7/amc-lp">22to7 – AMC: Let's Play!</Link>
+					</strong>{" "}
+					Set of weekly livestreams where I solve past AMC problems, taking questions live.
+				</li>
+			</ul>
+			<p>Projects that I consider completed and done:</p>
+			<ul>
+				<li>
+					<strong>
+						<a href="http://eartrain.surge.sh/">Eartrain.</a>
+					</strong>{" "}
+					A webapp that tests one's ability to identity notes from listening to random pitches in succession. Using a special
+					notation for half-flats and half-sharps, every octave contains 24 different pitches to be selected from.
+				</li>
+				<li>
+					<strong>
+						<a href="http://22to7.surge.sh/">22to7 – Prosol.</a>
+					</strong>{" "}
+					A 3-day mini math camp I did to introduce advanced mathematics problem solving skills to middle schoolers.
+				</li>
+			</ul>
+			<p>These are currently on a hiatus:</p>
+			<ul>
+				<li>
+					<strong>
+						<a href="https://www.npmjs.com/package/classical-music-tagger">CMT 2.</a>
+					</strong>{" "}
+					A tool to organize the metadata of classical music tracks, now with a clean database of compositions.
+				</li>
+			</ul>
 		</Layout>
 	);
 };
