8843d29c021a8a1029b8076e406a13342c89bfaf
Yuto Nishida <yuyu.nishida@gmail.com>
2020-06-12 18:27:48 -0700

    Clean up layout
    Removed boilerplate and unreachable code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -36,113 +36,48 @@ const Layout: React.FC<Args> = ({ location, title, children }) => {
 		},
 	];
 
-	//if (location.pathname === rootPath) {
-	if (true) {
-		header = (
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					marginBottom: rhythm(1.25),
-				}}
-			>
-				{tabs.map((tab, index) => {
-					const isFirst = index === 0;
-					const isHere = location.pathname === tab.to;
+	header = (
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				marginBottom: rhythm(1.25),
+			}}
+		>
+			{tabs.map((tab, index) => {
+				const isFirst = index === 0;
+				const isHere = location.pathname === tab.to;
 
-					return React.createElement(
-						isFirst ? "h1" : "h3",
+				return React.createElement(
+					isFirst ? "h1" : "h3",
+					{
+						style: {
+							...(isFirst && scale(1.5)),
+							marginTop: 0,
+							marginRight: rhythm(2),
+							marginBottom: 0,
+						},
+					},
+					React.createElement(
+						isHere ? "p" : Link,
 						{
 							style: {
-								...(isFirst && scale(1.5)),
-								marginTop: 0,
-								marginRight: rhythm(2),
+								color: `inherit`,
 								marginBottom: 0,
+								...(!isFirst &&
+									isHere && {
+										textDecoration: `underline`,
+									}),
 							},
+							to: tab.to,
 						},
-						React.createElement(
-							isHere ? "p" : Link,
-							{
-								style: {
-									color: `inherit`,
-									marginBottom: 0,
-									...(!isFirst &&
-										isHere && {
-											textDecoration: `underline`,
-										}),
-								},
-								to: tab.to,
-							},
-							tab.title
-						)
-					);
+						tab.title
+					)
+				);
+			})}
+		</div>
+	);
 
-					if (index === 0) {
-						return (
-							<h1
-								style={{
-									...scale(1.5),
-									marginTop: 0,
-									marginRight: rhythm(1.5),
-								}}
-							>
-								<Link
-									style={{
-										color: `inherit`,
-										...(location.pathname === tab.to && {
-											textDecoration: `underline`,
-										}),
-									}}
-									to={tab.to}
-								>
-									{tab.title}
-								</Link>
-							</h1>
-						);
-					} else {
-						return (
-							<h2
-								style={{
-									marginTop: 0,
-									marginRight: rhythm(1.5),
-								}}
-							>
-								<Link
-									style={{
-										color: `inherit`,
-										...(location.pathname === tab.to && {
-											textDecoration: `underline`,
-										}),
-									}}
-									to={tab.to}
-								>
-									{tab.title}
-								</Link>
-							</h2>
-						);
-					}
-				})}
-			</div>
-		);
-	} else {
-		header = (
-			<h3
-				style={{
-					fontFamily: `Montserrat, sans-serif`,
-					marginTop: 0,
-				}}
-			>
-				<Link
-					style={{
-						color: `inherit`,
-					}}
-					to={`/`}
-				>
-					{layoutTitle}
-				</Link>
-			</h3>
-		);
-	}
 	return (
 		<div
 			style={{
