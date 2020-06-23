5599bcec6eaaf45803e34696b83950a2fab10ce9
Yuto Nishida <yuyu.nishida@gmail.com>
2020-06-23 00:04:05 -0700

    Fix TeX naming conflict

--- src/components/Katex/TeX.js
+++ src/components/Katex/TeX.js
@@ -2,6 +2,6 @@ import React from "react";
 import "katex/dist/katex.min.css";
 import TeX from "@matejmazur/react-katex";
 
-const TeX = props => <TeX {...props}>{props.children}</TeX>;
+const Tex = props => <TeX {...props}>{props.children}</TeX>;
 
-export default TeX;
+export default Tex;
