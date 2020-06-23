e0910c4ed7e91711a5b2b0d80f52667a7437cdf8
Yuto Nishida <yuyu.nishida@gmail.com>
2020-06-22 20:59:53 -0700

    Revert "Add katex components"
    This reverts commit 31c5d06bcb5c7c595389862be627380e012afc4b.

--- src/components/Katex/InlineMath.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "katex/dist/katex.min.css";
-import { InlineMath } from "react-katex";
-
-const InlineMath = props => <InlineMath>{props.children}</InlineMath>;
-
-export default InlineMath;
