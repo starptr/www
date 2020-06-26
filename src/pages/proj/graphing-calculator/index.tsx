dec023f78e64b281ead5ab9b29fa75870bdc282d
Yuto Nishida <yuyu.nishida@gmail.com>
2020-06-26 15:16:58 -0700

    Remove unused gatsby Link import

--- src/pages/proj/graphing-calculator/index.tsx
+++ src/pages/proj/graphing-calculator/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import type { PageProps } from "gatsby";
 
 import Bio from "../../../components/bio";
