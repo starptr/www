e1cfafa084d4854f5db6d38fc25f4ac82ed6c4ae
Yuto Nishida <yuyu.nishida@gmail.com>
2020-07-20 23:05:49 -0700

    Link tags under rich post cards

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,20 +1,28 @@
 import React from "react";
+
+import LinkTag from "../components/LinkTag";
 import { rhythm } from "../utils/typography";
 
 const Tag = props => (
-	<small
+	<LinkTag
 		style={{
-			color: "rgb(68, 68, 68)",
-			backgroundColor: "rgb(221, 221, 221)",
-			cursor: "pointer",
-			whiteSpace: "pre",
-			paddingLeft: rhythm(0.2),
-			paddingRight: rhythm(0.2),
-			marginRight: rhythm(0.2),
+			textDecoration: "none",
 		}}
+		tag={props.children}
 	>
-		{props.children}
-	</small>
+		<small
+			style={{
+				color: "rgb(68, 68, 68)",
+				backgroundColor: "rgb(221, 221, 221)",
+				whiteSpace: "pre",
+				paddingLeft: rhythm(0.2),
+				paddingRight: rhythm(0.2),
+				marginRight: rhythm(0.2),
+			}}
+		>
+			{props.children}
+		</small>
+	</LinkTag>
 );
 
 export default Tag;
